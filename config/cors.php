5c194a63c1f7a127fc4e9b5a90ffc13a461523dc
Jhonatan-05 <jcanterohernandez71@correo.unicordoba.edu.co>
2025-06-16 09:47:54 -0500

    finish

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://frontend-maria.web.app/'],
+    'allowed_origins' => ['https://frontend-maria.web.app'],
 
     'allowed_origins_patterns' => [],
 
